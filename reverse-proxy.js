0085c83c0b471d412475c63fee7eb513c81a5c6b
ramzeta <ramiroperezsanz13@gmail.com>
2023-06-12 09:08:08 +0200

    update

--- reverse-proxy.js
+++ reverse-proxy.js
@@ -1,5 +1,11 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
+const LRU = require('lru-cache');
+
+const cache = new LRU({
+  max: 500, // Máximo número de elementos en la caché
+  maxAge: 60 * 1000, // Tiempo de vida máximo de cada elemento en milisegundos
+});
 
 // Configura las opciones del proxy
 const proxyOptions = {
@@ -12,6 +18,26 @@ const proxy = httpProxy.createProxyServer(proxyOptions);
 
 // Crea un servidor HTTP para recibir las solicitudes
 const server = http.createServer((req, res) => {
+  const cacheKey = req.url;
+
+  if (cache.has(cacheKey)) {
+    // Utilizar la respuesta almacenada en caché
+    const cachedResponse = cache.get(cacheKey);
+    res.writeHead(cachedResponse.statusCode, cachedResponse.headers);
+    res.end(cachedResponse.body);
+  } else {
+    // Obtener la respuesta del servidor y almacenarla en caché
+    fetchResponseFromServer(req, res)
+      .then((response) => {
+        cache.set(cacheKey, response);
+        res.writeHead(response.statusCode, response.headers);
+        res.end(response.body);
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      });
+  }
   // Redirige la solicitud al servidor de destino
   proxy.web(req, res);
 });
